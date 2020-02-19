5e24c2e7245f0190e640b4f2063d96e6ef31c304
郑尚姿 <33643927+zhengshangzi@users.noreply.github.com>
2020-02-19 11:01:05 +0800

    Add files via upload

---
+++ rectangle.js
@@ -0,0 +1,31 @@
+$(function() {
+    //get dom elem
+    var $width   = $('#width');
+    var $height  = $('#height');
+    var $calc    = $('#calc');
+    var $perimeter=$('#perimeter');
+    var $area    =$('#area');
+    /*calc button click event*/
+    $calc.click(function() {
+        //get value
+        var width  = Number($width.val());
+        var height = Number($height.val());
+        //calculate
+        var p      = roundFractional(width * 2 + height * 2, 2);
+        var a      = roundFractional(width * height, 2);
+        //output
+        $perimeter.val(p);
+        $area .val(a);
+    });
+    /**
+     * 小数点后面保留第 n 位
+     * 
+     * @param x 做近似处理的数
+     * @param n 小数点后第 n 位
+     * @returns 近似处理后的数 
+     */
+    function roundFractional(x, n) {
+        return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+    }
+});
+  
