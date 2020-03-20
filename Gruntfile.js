57fc1011c9f624730d468fd57229afb2ce014d06
郑尚姿 <33643927+zhengshangzi@users.noreply.github.com>
2020-03-20 11:36:30 +0800

    04

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,12 +26,36 @@ module.exports = function (grunt) {
           run: true,
           reporter: 'Dot'
         }
-      }
+      },
+      htmlmin: {
+        options: {
+          collapseWhitespace: true,
+          preserveLineBreaks: false
+        },
+        files: {
+          src: './index.html',
+          dest: 'dist/index.html'
+        }
+      },
+      cssmin: {
+        'dist/rectangle.css': 'rectangle.css'
+      },
+      uglify: {
+        release:{
+          files: {
+            'dist/rectangle.js': 'rectangle.js',
+            'dist/calc.js': 'calc.js'
+          }
+        }}
     });
     grunt.loadNpmTasks('grunt-contrib-csslint');
     grunt.loadNpmTasks('grunt-htmlhint');
     grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
     grunt.registerTask('default', ['mocha']);
+    grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
   };
