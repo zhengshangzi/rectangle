a8c7e64206faf28aeeab52648fd7b5be4eede7bf
郑尚姿 <33643927+zhengshangzi@users.noreply.github.com>
2020-03-20 16:59:49 +0800

    1.0

--- rectangle.js
+++ rectangle.js
@@ -1,80 +1,45 @@
+/* global Rectangle, validate: true */
 $(function() {
-    //get dom elem
-    var $width   = $('#width');
-    var $height  = $('#height');
-    var $calc    = $('#calc');
-    var $perimeter=$('#perimeter');
-    var $area    =$('#area');
-    /*calc button click event*/
-    $calc.click(function() {
-        // //get value
-        if(!validate('#width') || !validate('#height')) return;
-
-        // var width  = Number($width.val());
-        // var height = Number($height.val());
-        // //calculate
-        // var p      = roundFractional(width * 2 + height * 2, 10);
-        // var a      = roundFractional(width * height, 10);
-        // $perimeter.val(p);
-        // $area.val(a);
-        //output
-        var rect = rectangle();
-        $perimeter.val(rect.perimeter($width.val(), $height.val()));
-        $area.val(rect.area($width.val(), $height.val()));
-    });
+    var $width = $('#width'),
+        $height = $('#height'),
+        $btnCal = $('#calculate'),
+        $perimeter = $('#perimeter'),
+        $area = $('#area'),
+        $widthValidate = $('#width-validate'),
+        $heightValidate = $('#height-validate'),
+        isPassValidate = false;
+  
     $width.focusout(function() {
-   if(validate('#width'))
-     $width.select();
+      var result = validate($width.val());
+      isPassValidate = result.isOK;
+      if(!result.isOK) {
+        $widthValidate.html('宽度' + result.reason);
+        $width.select();
+      } else {
+        $widthValidate.html('');
+      }
+    });
+  
+    $height.focusout(function() {
+      var result = validate($height.val());
+      isPassValidate = result.isOK;
+      if(!result.isOK) {
+        $heightValidate.html('高度' + result.reason);
+        $height.select();
+      } else {
+        $heightValidate.html('');
+      }
+    });
+  
+    $btnCal.click(function(){
+      if(!isPassValidate) return;
+  
+      var w = $width.val(),
+          h = $height.val();
+  
+      var r = new Rectangle(w, h);
+  
+      $perimeter.val(r.perimeter());
+      $area.val(r.area());
+    });
   });
-  
-   $height.focusout(function() {
-     if( validate('#height'))
-     $height.select();
-   });
-    /**
-     * 小数点后面保留第 n 位
-     * 
-     * @param x 做近似处理的数
-     * @param n 小数点后第 n 位
-     * @returns 近似处理后的数 
-     */
-    function roundFractional(x, n) {
-        return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
-    }
-    /**
-   * 对字段进行数据合法性校验
-   *
-   * @param field 字段的 id
-   * @returns boolean 验证通过为 true，验证不通过为 false
-   */
-  function validate(field) {
-    //get dom error message
-    var $data    = $(field),
-        $message = $(field + '-validate'),
-        label    = $(field).attr('data-label');
-    //null
-    if($data.val() === '') {
-      $message.html(label + '不能为空！');
-      //设置焦点
-      $data.select();
-      return false;
-    }
-    //number
-    if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test($data.val())) {
-      $message.html(label + '必须是数值');
-      $data.select();
-      return false;
-    }
-    //>0 范围
-    if(window.Number($data.val()) < 0) {
-      //prompt error message
-      //return false
-      $message.html(label + '必须大于零');
-      $data.select();
-      return false;
-    }
-    $message.html('');
-    return true;
-  }
-});
-  
