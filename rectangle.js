5da74f63ac801355a90a779daa3f9130b6d2d47f
郑尚姿 <33643927+zhengshangzi@users.noreply.github.com>
2020-03-20 16:42:43 +0800

    Add files via upload

--- rectangle.js
+++ rectangle.js
@@ -8,6 +8,8 @@ $(function() {
     /*calc button click event*/
     $calc.click(function() {
         // //get value
+        if(!validate('#width') || !validate('#height')) return;
+
         // var width  = Number($width.val());
         // var height = Number($height.val());
         // //calculate
@@ -20,7 +22,15 @@ $(function() {
         $perimeter.val(rect.perimeter($width.val(), $height.val()));
         $area.val(rect.area($width.val(), $height.val()));
     });
-    
+    $width.focusout(function() {
+   if(validate('#width'))
+     $width.select();
+  });
+  
+   $height.focusout(function() {
+     if( validate('#height'))
+     $height.select();
+   });
     /**
      * 小数点后面保留第 n 位
      * 
@@ -31,5 +41,40 @@ $(function() {
     function roundFractional(x, n) {
         return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
     }
+    /**
+   * 对字段进行数据合法性校验
+   *
+   * @param field 字段的 id
+   * @returns boolean 验证通过为 true，验证不通过为 false
+   */
+  function validate(field) {
+    //get dom error message
+    var $data    = $(field),
+        $message = $(field + '-validate'),
+        label    = $(field).attr('data-label');
+    //null
+    if($data.val() === '') {
+      $message.html(label + '不能为空！');
+      //设置焦点
+      $data.select();
+      return false;
+    }
+    //number
+    if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test($data.val())) {
+      $message.html(label + '必须是数值');
+      $data.select();
+      return false;
+    }
+    //>0 范围
+    if(window.Number($data.val()) < 0) {
+      //prompt error message
+      //return false
+      $message.html(label + '必须大于零');
+      $data.select();
+      return false;
+    }
+    $message.html('');
+    return true;
+  }
 });
   
